a0e3720e554cf9752c7c909333081b0146c2d860
Shivraj Randive <shivrajrandive9281@gmail.com>
2026-01-28 18:44:42 +0530

    Delete ReedScraper directory

--- ReedScraper/Apify/ApifyModels.cs
+++
@@ -1,9 +0,0 @@
-namespace ReedScraper.Apify
-{
-    public class ApifyJobItem
-    {
-        public string url { get; set; } = string.Empty;   // initialize to avoid null
-        public string text { get; set; } = string.Empty;  // initialize to avoid null
-    }
-    
-}
